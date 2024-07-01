b6cc94c3ec137aa5c45b85da162ac4b6018d9eee
mariata06 <slinkywizard@mail.ru>
2024-07-01 20:41:40 +0300

    Add some changes for overflow

--- source/js/modules/init-slider.js
+++ source/js/modules/init-slider.js
@@ -1,25 +1,4 @@
 const initSlider = () => {
-
-  // const slides = document.querySelector('.slider__slides');
-  // const prevButton = document.querySelector('.slider__btn-prev');
-  // const nextButton = document.querySelector('.slider__btn-next');
-  // let currentIndex = 0;
-
-  // prevButton.addEventListener('click', () => {
-  //   currentIndex = (currentIndex === 0) ? slides.children.length - 1 : currentIndex - 1;
-  //   updateSlider();
-  // });
-
-  // nextButton.addEventListener('click', () => {
-  //   currentIndex = (currentIndex === slides.children.length - 1) ? 0 : currentIndex + 1;
-  //   updateSlider();
-  // });
-
-  // function updateSlider() {
-  //   slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-  // }
-
-
   const slides = document.querySelector('.slider__slides');
   const prevButton = document.querySelector('.slider__btn-prev');
   const nextButton = document.querySelector('.slider__btn-next');
@@ -53,7 +32,6 @@ const initSlider = () => {
       once: true,
     });
   }
-
 };
 
 export {initSlider};
